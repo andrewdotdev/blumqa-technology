d0754bbba0ad0bb866a4510f5c3bf61ec8805b37
Andrespolicrack <andrespoli071009@gmail.com>
2024-02-22 15:21:20 +0100

    fixed imports

--- src/functions/getById.ts
+++ src/functions/getById.ts
@@ -1,5 +1,5 @@
-const en = require('../../questions/en.json');
-const es = require('../../questions/es.json');
+import es from'../../questions/es.json'
+import en from'../../questions/en.json'
 
 /**
  * Get a q&a by the id
@@ -9,7 +9,7 @@ const es = require('../../questions/es.json');
 
 export function getById(lang: string, id: number) {
     if (!['es', 'en'].includes(lang)) throw new Error("Options of lang: 'en' or 'es'");
-    let language: [] = (lang === 'en' ? en : es);
+    let language = (lang === 'en' ? en : es);
     if(id < 1) id = 1
     if(language.length < id) id = language.length
     return language[id-1]
